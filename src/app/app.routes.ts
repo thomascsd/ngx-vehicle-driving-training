ca40fb700d7a3132f3b477c34abe5184bfb43b1c
thomascsd <thomascsd808@gmail.com>
2024-08-18 14:35:42 +0800

    add layout and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'days', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // { path: 'days', component: DaysForecastComponent },
+      // { path: 'locations', component: LocationForecastComponent }
+    ],
+  },
+];
