d05ad34789ccb8d34088da497d42a13a01c150e0
thomascsd <thomascsd808@gmail.com>
2024-10-27 15:59:05 +0800

    add customer and trainer component

---
+++ src/app/trainer/trainer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainer.component.html',
+  styleUrl: './trainer.component.scss'
+})
+export class TrainerComponent {
+
+}
