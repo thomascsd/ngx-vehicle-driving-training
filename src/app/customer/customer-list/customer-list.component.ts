2f0a2241f2ecf253bbbe0c6b20675338cf88ac02
Thomas Chang <thomascsd808@gmail.com>
2024-12-03 00:26:16 +0800

    add pagination

--- src/app/customer/customer-list/customer-list.component.ts
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { CustomerService } from '../../core/services/customer.service';
+import { Customer } from '../../core/models/customer';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [],
+  imports: [NgxPaginationModule],
   templateUrl: './customer-list.component.html',
-  styleUrl: './customer-list.component.scss'
+  styleUrl: './customer-list.component.scss',
 })
 export class CustomerListComponent {
+  customers: Signal<Customer[]>;
 
+  constructor(private customerService: CustomerService) {
+    this.customers = toSignal(this.customerService.getCustomers(), {
+      initialValue: [],
+    });
+  }
 }
