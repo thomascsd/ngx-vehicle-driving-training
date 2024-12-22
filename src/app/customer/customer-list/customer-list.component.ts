6b5ef664df0a0da004d05c99a834879e6d1cdc09
Thomas Chang <thomascsd808@gmail.com>
2024-12-22 16:35:10 +0800

    加入分頁及搜索功能

--- src/app/customer/customer-list/customer-list.component.ts
+++ src/app/customer/customer-list/customer-list.component.ts
@@ -1,18 +1,29 @@
 import { Component, Signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { CustomerService } from '../../core/services/customer.service';
 import { Customer } from '../../core/models/customer';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { NgxPaginationModule, PaginationInstance } from 'ngx-pagination';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [NgxPaginationModule],
+  imports: [NgxPaginationModule, CommonModule, FormsModule],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss',
 })
 export class CustomerListComponent {
   customers: Signal<Customer[]>;
+  name = '';
+  mobile = '';
+  birthday = '';
+
+  config: PaginationInstance = {
+    id: 'customer',
+    itemsPerPage: 10,
+    currentPage: 1,
+  };
 
   constructor(private customerService: CustomerService) {
     this.customers = toSignal(this.customerService.getCustomers(), {
